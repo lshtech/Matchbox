62b5abc7d7c94f264a92a41afa8fbf62b8521529
lshtech <elbebeta@gmail.com>
2024-10-09 10:21:48 -0500

    Add files via upload

---
+++ Matchbox/localization/en-us.lua
@@ -0,0 +1,58 @@
+return {
+    misc = {
+        challenge_names = {
+            c_mbox_globe = "Globe"
+        }
+    },
+    descriptions = {
+        Tarot = {
+            c_mbox_rebirth = {
+                name = "The Rebirth",
+                text = {
+                    "Enhances {C:attention}#1#{} selected",
+                    "card into a",
+                    "{C:attention}#2#"
+                }
+            },
+            c_mbox_existenz = {
+                name = "The Existenz",
+                text = {
+                    "Enhances {C:attention}#1#{}",
+                    "selected cards to",
+                    "{C:attention}#2#s"
+                }
+            },
+            c_mbox_healing = {
+                name = "The Healing",
+                text = {
+                   "Enhances {C:attention}#1#{} selected",
+                    "card into a",
+                    "{C:attention}#2#"
+                }
+            }
+        },
+        Enhanced = {
+			m_mbox_slimy = {
+                name = "Slimy Card",
+                    text = {
+                       "{C:green}#1# in #2#{} chance",
+                        "for {C:red}+#3#{} discard",
+                },
+           },
+           m_mbox_med = {
+                name = "Medical Card",
+                    text = {
+                       "Add copy without enhancement if this",
+                       "card is held in hand at end of round",
+                },
+           },
+           m_mbox_geo = {
+                name = "Geo Card",
+                    text = {
+                       "Change suit and rank",
+                       "of card when {C:attention}discarded",
+                },
+           },
+		},
+    }
+}
